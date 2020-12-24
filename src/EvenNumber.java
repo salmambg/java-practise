a903f7b673b1b461c2df3aa0a3c69edd7a3b30b9
Nazmul <nazmul@wearhealth.com>
2020-12-24 18:35:55 +0100

    feat: display even number

---
+++ src/EvenNumber.java
@@ -0,0 +1,9 @@
+
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        for (int number = 0; number <= 100 ; number+=2) {
+            System.out.println(number);
+        }
+    }
+}
