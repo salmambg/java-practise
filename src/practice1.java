5eb0f47a2e0d24ba423ba6ee73ee6748142de515
Nazmul <gsalmahossain@gmail.com>
2020-12-24 18:59:03 +0100

    feat: practice 1

---
+++ src/practice1.java
@@ -0,0 +1,12 @@
+
+public class practice1 {
+    public static void main (String[] args) {
+        for (int number=1; number <=20; number++) {
+            if (number == 10) {
+                System.out.println("Ten");
+            } else {
+                System.out.println(number);
+            }
+        }
+    }
+}
