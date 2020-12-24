231ebd30f0fc83c3496c0a6393599e702435d195
Nazmul <nazmul@wearhealth.com>
2020-12-24 18:25:31 +0100

    feat: print 1 to 10

---
+++ src/LoopTen.java
@@ -0,0 +1,8 @@
+
+public class LoopTen {
+    public static void main(String[] args) {
+        for (int number = 1; number <= 10; number++) {
+            System.out.println(number);
+        }
+    }
+}
