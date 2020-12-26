924d015f65763bd67fe4cbc5f7f75d096955e21e
Nazmul <gsalmahossain@gmail.com>
2020-12-26 06:45:46 +0100

    feat: practice 4

---
+++ src/Practice3.java
@@ -0,0 +1,8 @@
+
+public class Practice3 {
+    public static void main(String[] args){
+        for(int number = 1; number<= 10; number++){
+            System.out.println(number + " + " + number + " = " + number*2);
+        }
+    }
+}
