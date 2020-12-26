78f124403e603dfc79e9fb41e3a62606e3a9d0e8
Nazmul <gsalmahossain@gmail.com>
2020-12-26 05:42:01 +0100

    feat: multiplication of 10

---
+++ src/Counting10.java
@@ -0,0 +1,9 @@
+
+public class Counting10 {
+    public static void main(String[] args){
+        int counting = 10;
+        for (int number = 1; number<= 10; number++){
+            System.out.println(counting + " x " + number + " = " + counting * number);
+        }
+    }
+}
