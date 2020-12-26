90e559ef001c6f28cf7f3963fd0fc69bf1ac7076
salma <gsalmahossain@gmail.com>
2020-12-26 10:41:46 +0100

    feat: practice 6

--- src/LoopTen.java
+++ src/LoopTen.java
@@ -1,7 +1,10 @@
 
 public class LoopTen {
     public static void main(String[] args) {
-        for (int number = 1; number <= 10; number++) {
+        for (int number = 60; number <= 90; number++) {
+            if (number == 70 && number ==80) {
+                continue;
+            }
             System.out.println(number);
         }
     }
---
+++ src/Practice6.java
@@ -0,0 +1,11 @@
+
+public class Practice6 {
+    public static void main(String[] args){
+        for (int number = 60; number<=90; number++){
+            if(number == 70 || number == 80){
+                continue;
+            }
+            System.out.println(number);
+        }
+    }
+}
