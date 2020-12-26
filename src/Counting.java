7d6d8fd72b35bd20c4fb3fe68d745df791c4d07c
Nazmul <gsalmahossain@gmail.com>
2020-12-26 05:29:04 +0100

    feat: counting of 2

---
+++ src/Counting.java
@@ -0,0 +1,9 @@
+
+public class Counting {
+    public static void main(String[] args) {
+        int counting = 2;
+        for (int number = 1; number<= 10; number++){
+            System.out.println(counting + " x " + number + " = " + counting * number);
+        }
+    }
+}
--- src/Practice1.java
+++ src/Practice1.java
@@ -1,5 +1,5 @@
 
-public class practice1 {
+public class Practice1 {
     public static void main (String[] args) {
         for (int number=1; number <=20; number++) {
             if (number == 10) {
