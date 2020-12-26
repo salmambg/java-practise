17909fbd92895c2367704dfe830dd7f8d64b199c
Nazmul <gsalmahossain@gmail.com>
2020-12-26 07:00:42 +0100

    feat: practice 4

---
+++ src/Practice4.java
@@ -0,0 +1,12 @@
+
+public class Practice4 {
+    public static void main(String[] args){
+        int counting = 2;
+        for (int number = 1; number<= 10; number++){
+            if (number % 2 == 0){
+                System.out.println( counting + " x " + number + " = " + counting * number);
+            }
+        }
+
+    }
+}
