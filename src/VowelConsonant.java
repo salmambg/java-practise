c0c676051d4bfa3a8b42e9d31dbbbd8992fcdc93
salma <gsalmahossain@gmail.com>
2020-12-31 15:41:35 +0100

    feat: vowel consonant

---
+++ src/VowelConsonant.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner object = new Scanner(System.in);
+        System.out.println("Enter any letter");
+        char ch = object.next().charAt(0);
+        if (ch == 'a' || ch == 'e' || ch=='i' || ch== 'o' || ch== 'u'){
+            System.out.println("vowel");
+        }
+        else{
+            System.out.println("consonant");
+        }
+    }
+}
