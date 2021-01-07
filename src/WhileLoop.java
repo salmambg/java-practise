4e0ec78f52119f1a2f19c0f0d1ad9e2744d5aa2d
salma <gsalmahossain@gmail.com>
2021-01-07 10:40:28 +0100

    feat : while loop

---
+++ src/WhileLoop.java
@@ -0,0 +1,12 @@
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int number = 1;
+        while (number <= 10){
+            System.out.println("Welcome ");
+            number++;
+        }
+
+    }
+
+}
