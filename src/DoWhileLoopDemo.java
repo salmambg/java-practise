5f855143d80db6201f29afed95fecb4b42ec5516
salma <gsalmahossain@gmail.com>
2021-01-08 02:59:00 +0100

    feat: do while loop

---
+++ src/DoWhileLoopDemo.java
@@ -0,0 +1,10 @@
+
+public class DoWhileLoopDemo {
+    public static void main(String[] args) {
+        int number = 1;
+        do{
+            System.out.println(number);
+            number++;
+        }while( number<= 10);
+    }
+}
