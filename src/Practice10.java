41949cfb6f13285942384f9a14aa89a703b3dc5a
salma <gsalmahossain@gmail.com>
2021-01-08 03:14:31 +0100

    feat: practice 10

---
+++ src/Practice10.java
@@ -0,0 +1,11 @@
+
+public class Practice10 {
+    public static void main(String[] args) {
+        for(int number = 1; number<= 25; number++) {
+            if(number== 23){
+                continue;
+            }
+            System.out.println(number);
+        }
+    }
+}
