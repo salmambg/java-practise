8bc0d8b554fe5a1890d15bede013a2ecc7137f28
salma <gsalmahossain@gmail.com>
2021-01-08 03:09:37 +0100

    feat: practice 9

---
+++ src/Practice9.java
@@ -0,0 +1,12 @@
+
+public class Practice9 {
+    public static void main(String[] args) {
+        for(int number = 1; number <= 100; number++) {
+            if (number == 44) {
+                break;
+            }
+            System.out.println(number);
+        }
+
+    }
+}
