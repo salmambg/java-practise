95bb773f8473a20f53fd6a3c986770acc9273000
salma <gsalmahossain@gmail.com>
2021-01-29 18:18:39 +0600

    feat: small letter

---
+++ src/SmallLetter.java
@@ -0,0 +1,9 @@
+
+public class SmallLetter {
+    public static void main(String[] args) {
+        char c;
+        for(c = 'a'; c <= 'z'; c++) {
+            System.out.print(c + " ");
+        }
+    }
+}
