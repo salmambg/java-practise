152c0ab9de4332104ee20dd318e8b99c9293520b
salma <gsalmahossain@gmail.com>
2021-07-15 06:49:32 +0200

    task: array string and number

---
+++ src/ArrayPractice.java
@@ -0,0 +1,9 @@
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+       int[] numbers = {1,2,3,4,5,6,7,8};
+       for(int i : numbers) {
+           System.out. println(i);
+       }
+    }
+}
---
+++ src/ArrayString.java
@@ -0,0 +1,9 @@
+public class ArrayString {
+    public static void main (String[] args) {
+        String[] fruits = {"Mango", "Guava", "Jackfruit", "Strawberry", "Berry"};
+        for(String i : fruits){
+            System.out.println(i);
+
+        }
+    }
+}
