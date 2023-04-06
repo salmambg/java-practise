c56e8328291ac88711fb312c22650bd5bfa86243
salma <salmambg@gmail.com>
2023-04-06 13:31:52 +0200

    sum of digit

--- src/restart/Sum.java
+++ src/restart/Sum.java
@@ -1,7 +1,21 @@
 package restart;
 
+import java.util.Scanner;
+
 public class Sum {
     public static void main(String[] args) {
 
+        Scanner object= new Scanner(System.in);
+        int number, n, temp, sum=0;
+        System.out.print("enter any positive interger : ");
+        number = object.nextInt();
+        temp = number;
+
+        while(temp!= 0) {
+            n= temp%10;
+            sum = sum + n;
+            temp = temp/10;
+        }
+        System.out.println("Sum of Digit = "+ sum);
     }
 }
