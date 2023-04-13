08c34581cb84f2daaa6f19ffb1a04320da696335
salma <salmambg@gmail.com>
2023-04-13 22:38:12 +0200

    pattern practice 10

---
+++ src/restart/Patternprac10.java
@@ -0,0 +1,18 @@
+package restart;
+
+public class Patternprac10 {
+    public static void main(String[] args) {
+
+ //* * * * *
+ //* * * *
+// * * *
+// * *
+// *
+        for (int i=5;i>=1;i--) {
+            for (int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
