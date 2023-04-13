dfd9869891a6b6e7608e3cc21a83d03eda55b8c6
salma <salmambg@gmail.com>
2023-04-13 22:40:03 +0200

    pattern practice 9

--- src/restart/PatternPractice9.java
+++ src/restart/PatternPractice9.java
@@ -3,11 +3,11 @@ package restart;
 public class PatternPractice9 {
     public static void main(String[] args) {
 
- //     *
- //   * * *
+ // *
+ // * * *
  // * * * * *
- //   * * *
- //     *
+
+
 
       for (int i =1;i<=5; i+=2) {
           for(int j=1;j<=i;j++ ) {
