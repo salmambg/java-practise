126de5680404587b06762b2a36d7d98c2ce1c18d
salma <salmambg@gmail.com>
2023-04-19 14:31:14 +0200

    Array sorting

---
+++ src/array_demo/Sorting.java
@@ -0,0 +1,20 @@
+package array_demo;
+
+import java.util.Arrays;
+
+public class Sorting {
+    public static void main(String[] args) {
+        int[] number= {10,-3,8,5,6};
+
+        Arrays.sort(number);
+        System.out.print("Ascending: ");
+        for (int i=0;i<5;i++) {
+            System.out.print(" "+number[i]);
+        }
+        System.out.println();
+        System.out.print("Descending: ");
+        for (int i=4;i>=0;i--) {
+            System.out.print(" "+number[i]);
+        }
+    }
+}
