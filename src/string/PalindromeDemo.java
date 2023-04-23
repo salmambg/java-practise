c71a02e7811dc0d7a16f341bdabc73b54e47c4a0
salma <gsalmahossain@gmail.com>
2023-04-23 11:07:32 +0200

    String Palindrome

---
+++ src/string/PalindromeDemo.java
@@ -0,0 +1,16 @@
+package string;
+
+public class PalindromeDemo {
+    public static void main(String[] args) {
+        String s1 ="madam";
+        StringBuffer sb= new StringBuffer(s1);
+
+        String s2= sb.reverse().toString();
+
+        if (s1.equals(s2)) {
+            System.out.println("This is a palindrome");
+        }else {
+            System.out.println("Not a palindrome");
+        }
+    }
+}
