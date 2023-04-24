2cbe12a6cd01ac386044456569712da734dff9c4
salma <gsalmahossain@gmail.com>
2023-04-24 20:55:35 +0200

    Random Number Generator

---
+++ src/restart/RandomDemo.java
@@ -0,0 +1,12 @@
+package restart;
+
+import java.util.Random;
+
+public class RandomDemo {
+    public static void main(String[] args) {
+        Random rand= new Random();
+        int randomNumber= rand.nextInt(10); //It will generate any number from 0 to 9 in every run
+        System.out.println(randomNumber);
+
+    }
+}
