3ae57a413e357cecc03a17ff69acafe31197a3c6
salma <gsalmahossain@gmail.com>
2023-04-25 15:32:34 +0200

    Use of Constructor

--- src/OOP/Teacher.java
+++ src/OOP/Teacher.java
@@ -1,6 +1,13 @@
 package OOP;
 
 public class Teacher {
+
+    Teacher(String n,String g,String p) {//there is no return type
+        name =n;
+        gender=g;
+        phone =p;
+
+    }
     String name,gender;
     String phone;
 
--- src/OOP/Test.java
+++ src/OOP/Test.java
@@ -5,16 +5,26 @@ public class Test {
 //        Teacher teacher1;//object declare
 //        teacher1=new Teacher();//object create
 
-        Teacher teacher1=new Teacher();
-        teacher1.setInformation("Salma Akter","Female","0167453575");
-        teacher1.displayInformation();
+//        Teacher teacher1=new Teacher();
+//        teacher1.setInformation("Salma Akter","Female","0167453575");
+//        teacher1.displayInformation();
+//
+//        System.out.println();//to make a new line you can also write this in method
+//
+//        Teacher teacher2=new Teacher();
+//        teacher2.setInformation("G M Nazmul","male","55234388");
+//        teacher2.displayInformation();
 
-        System.out.println();//to make a new line you can also write this in method
-
-        Teacher teacher2=new Teacher();
-        teacher2.setInformation("G M Nazmul","male","55234388");
-        teacher2.displayInformation();
+// Here I can use constructor to initialize the object when the object is created with new operator
+        //we don't need to call constructor.
 
 
+          Teacher teacher1=new Teacher("Salma Akter","Female","0167453575");
+          teacher1.displayInformation();
+
+          System.out.println();//to make a new line you can also write this in method
+
+          Teacher teacher2=new Teacher("G M Nazmul","male","55234388");
+          teacher2.displayInformation();
     }
 }
