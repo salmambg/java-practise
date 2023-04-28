9de03937e74470e24cd829a868dc69cda6764d29
salma <gsalmahossain@gmail.com>
2023-04-28 17:35:45 +0200

    Math Class

---
+++ src/OOP/MathDemo.java
@@ -0,0 +1,24 @@
+package OOP;
+
+public class MathDemo {
+    public static void main(String[] args) {
+
+        int x= Math.abs(-4);
+        System.out.println(x);
+
+        double y=Math.abs(-9.5);
+        System.out.println(y);
+
+        //or we can do this thing in sout directly.
+        System.out.println(Math.sqrt(25.0));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.PI);
+        System.out.println(Math.log(1));
+        System.out.println(Math.exp(1));
+        System.out.println(Math.max(5,9));
+        System.out.println(Math.min(2.4,-6.7));
+        System.out.println(Math.ceil(-5.6));
+        System.out.println(Math.floor(-5.6));
+
+    }
+}
