5435424676f83b631aee37e9eaedf6b5c9aced3e
salma <gsalmahossain@gmail.com>
2023-04-28 17:51:13 +0200

    Method Overloading

---
+++ src/OOP/OverloadingTest.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class OverloadingTest {
+    public static void main(String[] args) {
+        Overloading x= new Overloading();
+        x.add();
+        x.add(5,10);
+        x.add(5,10,15);
+        x.add(3.4,2.5);
+
+    }
+}
