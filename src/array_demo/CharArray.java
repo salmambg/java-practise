e3c62ef5d113c130a6e4a8121a0f3ceb84d868c2
salma <gsalmahossain@gmail.com>
2023-04-29 18:13:13 +0200

    Character Array

---
+++ src/array_demo/CharArray.java
@@ -0,0 +1,10 @@
+package array_demo;
+
+public class CharArray {
+    public static void main(String[] args) {
+        char[] charArray = {'a', 'b', 'c', 'd'};
+        for (char c : charArray) {
+            System.out.println(c);
+        }
+    }
+}
